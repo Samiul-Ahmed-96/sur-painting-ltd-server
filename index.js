20a0fd76026bbbfcf0f83591efd6428e7123005e
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-04-07 15:06:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvzsfvb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const database = client.db("sur_painting_ltd");
+    const servicesCollection = database.collection("services");
+    const projectsCollection = database.collection("projects");
+    const contactListCollection = database.collection("contact-list");
+    //Get Services
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send({ status: true, data: services });
+    });
+    //Get Projects
+    app.get("/projects", async (req, res) => {
+      const cursor = projectsCollection.find({});
+      const projects = await cursor.toArray();
+      res.send({ status: true, data: projects });
+    });
+    //Get Projects
+    app.get("/contacts", async (req, res) => {
+      const cursor = contactListCollection.find({});
+      const contacts = await cursor.toArray();
+      res.send({ status: true, data: contacts });
+    });
+    //Get Projects
+    app.get("/projects", async (req, res) => {
+      const cursor = projectsCollection.find({});
+      const projects = await cursor.toArray();
+      res.send({ status: true, data: projects });
+    });
+    //Get Single Service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await servicesCollection.findOne({ _id: ObjectId(id) });
+      res.send({ status: true, data: result });
+    });
+    //Get Single Projects
+    app.get("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await projectsCollection.findOne({ _id: ObjectId(id) });
+      res.send({ status: true, data: result });
+    });
+    //Add Project Api
+    app.post("/projects", async (req, res) => {
+      const project = req.body;
+      const result = await projectsCollection.insertOne(project);
+      res.send({ status: true, data: result });
+    });
+    //Add Project Api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      res.send({ status: true, data: result });
+    });
+    //Add Project Api
+    app.post("/contacts", async (req, res) => {
+      const contact = req.body;
+      const result = await contactListCollection.insertOne(contact);
+      res.send({ status: true, data: result });
+    });
+    //Delete single item from Services Api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json({ status: true, data: result });
+    });
+    //Delete single item from projects Api
+    app.delete("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await projectsCollection.deleteOne(query);
+      res.json({ status: true, data: result });
+    });
+    //Delete single item from contact Api
+    app.delete("/contacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await contactListCollection.deleteOne(query);
+      res.json({ status: true, data: result });
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Sur Painting Ltd");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
